e4f9b5a43234e6c1c64c180f07cff417ddb776f8
S Taubman <akoga3@gatech.edu>
2023-12-04 18:24:13 -0500

    testing sanity integration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        dark: "#1a202c",
+        darky: "#1a202c",
+        mid: "#1a202c",
+        lighty: "#1a202c",
+        light: "#1a202c",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
