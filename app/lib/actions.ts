9ec6b36b4cb5a6590e5d860f4680720130b1921c
S Taubman <akoga3@gatech.edu>
2023-12-07 15:15:38 -0500

    cache no store and metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,20 +1,18 @@
 import { book, bookCover, page } from "@/app/lib/definitions";
 import { client } from "@/app/lib/utils";
 
-export const dynamic = "force-dynamic";
-
 export async function GetAllBookCovers(): Promise<bookCover[]> {
   const query = `*[_type == "book"]{
     title, slug, image
   }`;
-  const books: book[] = await client.fetch(query);
+  const books: book[] = await client.fetch(query, { cache: "no-store" });
 
   return books;
 }
 
 export async function GetAllBooks(): Promise<book[]> {
   const query = `*[_type == "book"]`;
-  const books: book[] = await client.fetch(query);
+  const books: book[] = await client.fetch(query, { cache: "no-store" });
 
   return books;
 }
@@ -27,14 +25,14 @@ export async function GetBookData(slug: string): Promise<book> {
     }
   }
   `;
-  const book = await client.fetch(query);
+  const book = await client.fetch(query, { cache: "no-store" });
 
   return book[0];
 }
 
 export async function GetPageData(slug: string): Promise<page> {
   const query = `*[_type == "page" && slug.current == "${slug}"]`;
-  const page = await client.fetch(query);
+  const page = await client.fetch(query, { cache: "no-store" });
 
   return page[0];
 }
@@ -43,13 +41,12 @@ export async function GetRecentPost(): Promise<string[]> {
   const query = `*[_type == "page"] | order(date desc)[0]{
     title, slug, _id
   }`;
-  const page = await client.fetch(query);
+  const page = await client.fetch(query, { cache: "no-store" });
 
   const booksQuery = `*[_type == "book" && references("${page._id}")][0]{
     slug
   }`;
-  const books = await client.fetch(booksQuery);
+  const books = await client.fetch(booksQuery, { cache: "no-store" });
 
   return [page.title, page.slug.current, books.slug.current];
-  // return ["", "", ""];
 }
