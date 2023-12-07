bd1b1c63ae2307bd1ee49a052928f4a5535256df
S Taubman <akoga3@gatech.edu>
2023-12-07 00:01:59 -0500

    yeargh

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-//https://github.com/Automattic/node-canvas/issues/1779
-
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
